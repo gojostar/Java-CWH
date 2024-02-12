bc2257e362141eec806adac2df12e59a1dfec738
Magneto-101 <mohitcodes99@gmail.com>
2024-02-12 17:58:09 +0530

    updated new line

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main
     public static void main(String[]args)
         {
             System.out.println("hello world\n");
-            System.out.println("This is my first java program");
+            System.out.println("This is my first java program\n");
         }
 }
