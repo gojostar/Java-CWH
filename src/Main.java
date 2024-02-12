3b56253d17f8ad252875e91cc3910b4c464c7932
Magneto-101 <mohitcodes99@gmail.com>
2024-02-12 22:18:15 +0530

    ubuntu setup

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 public class Main
 {
     public static void main(String[]args)
-        {
-            System.out.println("hello world\n");
-            System.out.println("This is my first java program\n");
-        }
+    {
+        System.out.println("Hello world !");
+    }
 }
