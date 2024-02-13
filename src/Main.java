4762c6a0e8ab4111d761a46c0f5a9db4ee694b6d
Magneto-101 <mohitcodes99@gmail.com>
2024-02-13 23:17:20 +0530

    Added sum of three numbers

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main
     {
         System.out.println("Hello World");
         System.out.println("This is mohit yadav");
+
+        int num1 = 2;
+        int num2 = 34;
+        int num3 = 23;
+        int sum = (num1 + num2 + num3);
+        System.out.println(sum);
     }
 }
