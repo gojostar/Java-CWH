4905fef2cf6630f67d4543fdabf7fade489f96e9
Mohit Yadav <135860609+gojostar@users.noreply.github.com>
2024-04-12 10:14:41 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main
         int num1 = 2;
         int num2 = 34;
         int num3 = 23;
-        int num4 = 35;
+        int num4 = 48;
         int sum = (num1 + num2 + num3 +num4);
         System.out.println(sum);
     }
