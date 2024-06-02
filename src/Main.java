1e0a79473b592f990306c68e147df944b8d26abf
Mohit Yadav <135860609+gojostar@users.noreply.github.com>
2024-06-02 09:42:36 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[]args)
     {
         System.out.println("Hello World");
-        System.out.println("This is mohit yadav");
+        System.out.println("This is Gojostar");
 
         int num1 = 2;
         int num2 = 34;
